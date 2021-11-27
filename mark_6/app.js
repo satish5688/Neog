13393e394389b9b677d74b4ce48b74d2baa01624
satish5688 <satishmungusmare5688@gmail.com>
2021-11-27 21:27:45 +0530

    stanslate input output

---
+++ mark_6/app.js
@@ -0,0 +1,11 @@
+var btntranslate=document.querySelector("#btn_translate");
+var textinput=document.querySelector("#txt-input");
+var outputDiv=document.querySelector("#output");
+
+
+function clickHandler(){
+    outputDiv.innerText="translate "+textinput.value
+    
+}
+
+btntranslate.addEventListener("click", clickHandler)
